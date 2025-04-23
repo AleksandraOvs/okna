a345cdaf00967c716b0a0c02f3c36940159579dd
aleksandra <aleksandra.ovsia@gmail.com>
2025-04-23 15:41:22 +0300

    eds

--- wp-content/themes/divo/page-osteklenie.php
+++ wp-content/themes/divo/page-osteklenie.php
@@ -3,30 +3,29 @@
 */
 ?>
 
-<?php //get_header() ?>
-
-<!-- <div data-noindex> -->
-<?php // get_template_part('template-parts/hero-block') ?>
-<?php // get_template_part('template-parts/services') ?>
-<?php // get_template_part('template-parts/works') ?>
-<?php // get_template_part('template-parts/types') ?>
-<?php // get_template_part('template-parts/repeat-blocks/order') ?>
-<?php // get_template_part('template-parts/repeat-blocks/express') ?>
-<?php // get_template_part('template-parts/repeat-blocks/catalog') ?>
-<?php // get_template_part('template-parts/repeat-blocks/nums') ?>
-<?php // get_template_part('template-parts/repeat-blocks/pays') ?>
-<?php // get_template_part('template-parts/repeat-blocks/benefits') ?>
-<?php // get_template_part('template-parts/repeat-blocks/production') ?>
-<?php // get_template_part('template-parts/repeat-blocks/complect') ?>
-<?php // get_template_part('template-parts/repeat-blocks/delivery') ?>
-<?php // get_template_part('template-parts/offer') ?>
-<?php // get_template_part('template-parts/repeat-blocks/faq') ?>
-<?php // get_template_part('template-parts/repeat-blocks/feedback') ?>
-<!-- </div> -->
-
 <?php get_header() ?>
 
-<div data-ajax-block="hero-block"></div>
+
+<?php get_template_part('template-parts/hero-block') ?>
+<?php get_template_part('template-parts/services') ?>
+<?php get_template_part('template-parts/works') ?>
+<?php get_template_part('template-parts/types') ?>
+<?php get_template_part('template-parts/repeat-blocks/order') ?>
+<?php get_template_part('template-parts/repeat-blocks/express') ?>
+<?php get_template_part('template-parts/repeat-blocks/catalog') ?>
+<?php get_template_part('template-parts/repeat-blocks/nums') ?>
+<?php get_template_part('template-parts/repeat-blocks/pays') ?>
+<?php get_template_part('template-parts/repeat-blocks/benefits') ?>
+<?php get_template_part('template-parts/repeat-blocks/production') ?>
+<?php get_template_part('template-parts/repeat-blocks/complect') ?>
+<?php get_template_part('template-parts/repeat-blocks/delivery') ?>
+<?php get_template_part('template-parts/offer') ?>
+<?php get_template_part('template-parts/repeat-blocks/faq') ?>
+<?php get_template_part('template-parts/repeat-blocks/feedback') ?>
+
+
+
+<!-- <div data-ajax-block="hero-block"></div>
 <div data-ajax-block="services"></div>
 <div data-ajax-block="works"></div>
 <div data-ajax-block="types"></div>
@@ -41,7 +40,7 @@
 <div data-ajax-block="repeat-blocks/delivery"></div>
 <div data-ajax-block="offer"></div>
 <div data-ajax-block="repeat-blocks/faq"></div>
-<div data-ajax-block="repeat-blocks/feedback"></div>
+<div data-ajax-block="repeat-blocks/feedback"></div> -->
 
 <div class="container">
     <?php the_content() ?>
---
+++ wp-content/themes/divo/page-test.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Template name: test
+ */
+
+ get_header();
+
+ get_template_part('template-parts/repeat-blocks/order');
+
+ get_footer();
